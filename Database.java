4b569e6582a5f0eed85d1e14b498a825210c4106
fauzan04 <115762626+fauzan04@users.noreply.github.com>
2022-12-03 20:16:49 +0700

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,17 @@
+public class Database {
+    
+    String menu;
+    String showMenu;
+    String searchMenu;
+
+    public void setMenu(String menu){
+        this.menu = menu;
+    }
+    public void setShowMenu(String showMenu){
+        this.showMenu = showMenu;
+    }
+    public void setSearchMenu(String searchMenu){
+        this.searchMenu = searchMenu;
+    }
+}
+
